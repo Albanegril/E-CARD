8a2f12bbf4ae3feaf345a982a35ab6e1f33a2c92
Albanegril <albane.gril@free.fr>
2018-11-15 17:56:35 +0100

    Update RA

---
+++ Assets/Scripts/AAAA.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class AAAA : MonoBehaviour {
+
+    string btnName;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    void Update()
+    {
+        if ((Input.touchCount > 0) && (Input.touches[0].phase == TouchPhase.Began))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit Hit;
+
+            if (Physics.Raycast(ray, out Hit))
+            {
+                btnName = Hit.transform.name;
+                switch (btnName)
+                {
+                    case "telephone":
+                        Application.OpenURL("https://www.google.com");
+                        break;
+
+                    case "mail":
+                        Application.OpenURL("https://gmail.com");
+                        break;
+
+                    case "Twitter":
+                        Application.OpenURL("https://www.twitter.com");
+                        break;
+
+                    case "fb":
+                        Application.OpenURL("https://www.facebook.com");
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+	}
+}
--- Assets/Scripts/EditButtonCard.cs
+++ Assets/Scripts/EditButtonCard.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class EditButtonCard : MonoBehaviour {
+    public TMP_InputField ChampsSaisie;
+    public TextMeshProUGUI TextBox;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        
+        TextBox = new TextMeshProUGUI();
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void Facebook()
+    {
+        // set input text box active, 
+        // ChampsSaisie.SetActive();
+        // enter fb name or link // drag&drop link
+        TextBox.text = "Saisisez votre nom fb ou un lien";
+        // register link in card link DB
+        // afficher la saisie
+                   TextBox.text = ChampsSaisie.text;
+
+    }
+
+
+
 }
